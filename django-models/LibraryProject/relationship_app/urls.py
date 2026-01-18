700b342d354de8aefa8a6a853b445d06cbebe3f4
Mwiteneza emelyne <mwitenezaemelyne184@gmail.com>
2026-01-18 17:57:54 +0200

    Initial commit - Django LibraryProject with custom permissions

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import list_books, LibraryDetailView, register
 from .views import admin_view, librarian_view, member_view
+from . import views
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
@@ -18,4 +19,10 @@ urlpatterns = [
     path('admin-view/', admin_view, name='admin_view'),
     path('librarian-view/', librarian_view, name='librarian_view'),
     path('member-view/', member_view, name='member_view'),
+    
+    path('books/add/', views.add_book, name='add_book'),
+    path('books/edit/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('books/delete/<int:book_id>/', views.delete_book, name='delete_book'),
+    
+    
 ]
