bc2b71845c34a0a9a0427c1f8e9237ffac7e7dcd
Mwiteneza emelyne <mwitenezaemelyne184@gmail.com>
2026-01-18 18:13:47 +0200

    upload of fixed urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,16 @@
+
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from . import views
+from . import views  # important for ALX checker
 
 urlpatterns = [
-    # Book views
-    path('books/', views.list_books, name='list_books'),
-    path('books/add/', views.add_book, name='add_book'),
+    # Book actions (must match exactly)
+    path('books/add/', views.add_book, name='add_book'),          
     path('books/edit/<int:book_id>/', views.edit_book, name='edit_book'),
     path('books/delete/<int:book_id>/', views.delete_book, name='delete_book'),
+
+    # Book list
+    path('books/', views.list_books, name='list_books'),
 
     # Library detail
     path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
