a89eff1097ee50e787b58d08ceaf461363dffbd4
Mwiteneza emelyne <mwitenezaemelyne184@gmail.com>
2026-01-18 18:09:05 +0200

    upload of fixed views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import user_passes_test, permission_required
-
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required 
 from bookshelf.models import Book
 from .models import Library
 from .forms import BookForm
