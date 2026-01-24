740fe54cee77dfa39d019ccbc9293c4fb4a82541
Mwiteneza emelyne <mwitenezaemelyne184@gmail.com>
2026-01-24 11:13:35 +0200

    upload of custom user model implemented

--- LibraryProject/relationship_app/admin.py
+++ LibraryProject/relationship_app/admin.py
@@ -2,21 +2,13 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-
-    # Add custom fields in admin
     fieldsets = UserAdmin.fieldsets + (
-        ('Additional Information', {
-            'fields': ('date_of_birth', 'profile_photo'),
-        }),
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
     )
-
     add_fieldsets = UserAdmin.add_fieldsets + (
-        ('Additional Information', {
-            'fields': ('date_of_birth', 'profile_photo'),
-        }),
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
     )
 
-    list_display = ('username', 'email', 'is_staff', 'is_active')
+admin.site.register(CustomUser, CustomUserAdmin)
