011d6dc3757b0099c486b2147ae37d845c3bcd44
Mwiteneza emelyne <mwitenezaemelyne184@gmail.com>
2026-02-08 16:13:05 +0200

    fix of api/urls.py and views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,61 +1,31 @@
-from rest_framework import generics, permissions,filters
+
+from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
 
-# Read-only views
-
+# Read-only (public)
 class BookListView(generics.ListAPIView):
-    
-    """
-    GET /api/books/
-    List all books in the database.
-    Accessible by anyone (public view).
-    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.AllowAny]
-    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
-    search_fields = ['title', 'author__name']
-    ordering_fields = ['publication_year', 'title']
-
 
 class BookDetailView(generics.RetrieveAPIView):
-    """
-    GET /api/books/<id>/
-    Retrieve a single book by its ID.
-    Accessible by anyone (public view).
-    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.AllowAny]
 
-# Authenticated CRUD views
+# Authenticated CRUD
 class BookCreateView(generics.CreateAPIView):
-    """
-    POST /api/books/create/
-    Create a new book.
-    Only accessible by authenticated users.
-    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticated]
 
 class BookUpdateView(generics.UpdateAPIView):
-    """
-    PUT /api/books/<id>/update/
-    Update an existing book by ID.
-    Only accessible by authenticated users.
-    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticated]
 
 class BookDeleteView(generics.DestroyAPIView):
-    """
-    DELETE /api/books/<id>/delete/
-    Delete a book by ID.
-    Only accessible by authenticated users.
-    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticated]
