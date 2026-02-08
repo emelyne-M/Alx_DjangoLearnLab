7622d13204c9a261a4f4405bb01bd55e0ec23c0d
Mwiteneza emelyne <mwitenezaemelyne184@gmail.com>
2026-02-08 14:50:32 +0200

    upload of Setup Django REST API

--- advanced_api_project/api/models.py
+++ advanced_api_project/api/models.py
@@ -1,18 +1,30 @@
+
 from django.db import models
 
 
 class Author(models.Model):
-    name = models.CharField(max_length=255)
+    """
+    Author model represents a writer who can have multiple books.
+    One Author -> Many Books
+    """
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
+
 class Book(models.Model):
-    title = models.CharField(max_length=255)
+    """
+    Book model represents a book written by an author.
+    Each book belongs to one author.
+    """
+    title = models.CharField(max_length=200)
     publication_year = models.IntegerField()
+
+    # ForeignKey establishes one-to-many relationship
     author = models.ForeignKey(
         Author,
-        related_name='books', 
+        related_name='books',
         on_delete=models.CASCADE
     )
 
