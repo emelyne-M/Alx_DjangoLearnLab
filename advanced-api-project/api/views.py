3c0b6d255c64486078e5819d938a2af5f3244fe6
Mwiteneza emelyne <mwitenezaemelyne184@gmail.com>
2026-02-08 16:50:55 +0200

    ALX-ready: complete Django REST API with permissions and urls

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,5 @@
-
-from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework import generics, filters
 from .models import Book
 from .serializers import BookSerializer
 
