21f56a77c0849de44b101795d9695f74b1989049
Mwiteneza emelyne <mwitenezaemelyne184@gmail.com>
2026-02-08 16:43:38 +0200

    upload of api/urls.py and views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,45 +4,30 @@ from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnl
 from .models import Book
 from .serializers import BookSerializer
 
-# ------------------------
 # READ-ONLY VIEWS
-# ------------------------
 class BookListView(generics.ListAPIView):
-    """
-    GET /api/books/
-    List all books.
-    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]  # ALX wants this
+    permission_classes = [IsAuthenticatedOrReadOnly]
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['title', 'author__name']
     ordering_fields = ['publication_year', 'title']
 
-
 class BookDetailView(generics.RetrieveAPIView):
-    """
-    GET /api/books/<id>/
-    Retrieve a single book.
-    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
-# ------------------------
-# AUTHENTICATED CRUD VIEWS
-# ------------------------
+# CRUD VIEWS
 class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
 
-
 class BookUpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
-
 
 class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
